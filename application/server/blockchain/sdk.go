717744876ab7ddd23117b4fb1c6d9cc39489b884
SamSixFlower <124448632+SamSixFlower@users.noreply.github.com>
2023-09-10 21:17:53 +0800

    Update sdk.go

--- application/server/blockchain/sdk.go
+++ application/server/blockchain/sdk.go
@@ -12,7 +12,7 @@ var (
 	configPath    = "config.yaml"                                // 配置文件路径
 	channelName   = "appchannel"                                 // 通道名称
 	user          = "Admin"                                      // 用户
-	chainCodeName = "fabric-realty"                              // 链码名称
+	chainCodeName = "fabric-suyuan"                              // 链码名称
 	endpoints     = []string{"peer0.jd.com", "peer0.taobao.com"} // 要发送交易的节点
 
 	//configPath    = "config-local-dev.yaml"                      // 配置文件路径(本地开发时使用)
